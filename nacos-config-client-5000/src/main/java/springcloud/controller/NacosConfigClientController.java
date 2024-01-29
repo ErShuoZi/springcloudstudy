730e9f9a4b1aef99dc55cb06b2b2121910eeb54f
ErShuoZi <liushuo2951@163.com>
2024-01-29 14:54:06 +0800

    Nacos配置 配置中心

---
+++ nacos-config-client-5000/src/main/java/springcloud/controller/NacosConfigClientController.java
@@ -0,0 +1,28 @@
+package springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class NacosConfigClientController {
+    //Client 会拉取配置中心的的数据，在bootstrap.yaml文件中配置过要拉取哪个数据
+    @Value("${config.ip}")
+    private String configIp;
+    @Value("${config.name}")
+    private String configName;
+
+    @GetMapping("/nacos/config/ip")
+    public String getConfigIp() {
+        return configIp;
+    }
+
+    @GetMapping("/nacos/config/name")
+    public String getConfigName() {
+        return configName;
+    }
+
+
+}
