521adfde2432796374817e98d2452147f56c3266
ErShuoZi <liushuo2951@163.com>
2024-01-29 15:09:21 +0800

    Nacos配置自动刷新配置信息

--- nacos-config-client-5000/src/main/java/springcloud/controller/NacosConfigClientController.java
+++ nacos-config-client-5000/src/main/java/springcloud/controller/NacosConfigClientController.java
@@ -2,11 +2,13 @@ package springcloud.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
+@RefreshScope // 实现配置信息自动刷新
 public class NacosConfigClientController {
     //Client 会拉取配置中心的的数据，在bootstrap.yaml文件中配置过要拉取哪个数据
     @Value("${config.ip}")
